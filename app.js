b42cdb9a0956a84e3fabd6c51ccbe54ce7ffd2e6
Canberk <canberkgungormez@hotmail.com>
2023-05-18 16:28:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// required packages
+
+const express = require("express");
+const { default: fetch } = require("node-fetch");
+
+require("dotenv").config();
+
+//create express server
+
+const app = express();
+
+//port num
+
+const PORT = process.env.PORT || 3000;
+
+// set template engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//needed to parse html data for post
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/convert-mp3", async (req, res) => {
+  const videoId = req.body.videoID;
+  if (videoId === undefined || videoId === "" || videoId === null) {
+    return res.render("index", { success: false, message: "Link giriniz." });
+  } else {
+    const fetchAPI = await fetch(
+      `https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": process.env.API_HOST,
+        },
+      }
+    );
+    const fetchResponse = await fetchAPI.json();
+
+    if (fetchResponse.status === "ok")
+      return res.render("index", {
+        success: true,
+        song_title: fetchResponse.title,
+        song_link: fetchResponse.link,
+      });
+    else return res.render("index", { success: false, message: fetchAPI.msg });
+  }
+  console.log(videoId);
+});
+
+// Start server
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
